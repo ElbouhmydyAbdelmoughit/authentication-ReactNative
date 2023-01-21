27e90b1af04d706db0e1b9f2734b7d70fcb7f3b7
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-01-21 16:23:55 +0000

    add button login and sigup in home page

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,62 @@
 import React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import { Text, StyleSheet, View, TouchableOpacity, Alert } from 'react-native';
 import Background from './Background';
 
 const Home = () => {
+ 
   return (
     <Background>
       <View style={styles.container}>
         <Text style={styles.text}>Let's start</Text>
-        <Text style={styles.second_text}>Coding</Text>
+        <Text style={styles.text}>Coding</Text>
+        <TouchableOpacity style={styles.Button_login} >
+          <Text style={styles.button_Text_login}>Log in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.Button_signup} >
+          <Text style={styles.button_Text_signup}>Sign up</Text>
+        </TouchableOpacity>
       </View>
     </Background>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     marginHorizontal: 35,
-    marginVertical: 100
+    marginVertical: 130,
+    marginBottom:300
   },
-  text:{
-    color: "white",
-    fontSize: 64,
-    fontFamily:"",
+  text: {
+    color: 'white',
+    fontSize: 60,
+    fontFamily:"Poppins"
   },
-  second_text:{
+  Button_login:{
+    alignItems: 'center',
+    backgroundColor: "green",
+    borderRadius: 100,
+    width: 300,
+    paddingVertical: 7,
+    marginVertical:10,
+  },
+  Button_signup:{
+    alignItems: 'center',
+    backgroundColor: "white",
+    borderRadius: 100,
+    width: 300,
+    paddingVertical: 7,
+    marginVertical:10,
+  },
+  button_Text_login:{
     color: "white",
-    fontSize: 64,
-    fontFamily:"",
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  button_Text_signup:{
+    color: "green",
+    fontSize: 25,
+    fontWeight: 'bold',
   }
-})
+});
 
 export default Home;
